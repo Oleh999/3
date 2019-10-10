30d8e20bbdba450b2afb8813e27604e0f03c65d2
Oleh <52893252+Oleh999@users.noreply.github.com>
2019-10-10 14:49:55 +0300

    Update editUser.js

--- controllers/user/editUser.js
+++ controllers/user/editUser.js
@@ -2,12 +2,11 @@ const {provider} = require('../../database');
 
 module.exports = async (req, res) => {
     try {
-        // const {  } = req.params;
         const {user_id,name,email,password} = req.body;
 
-        const query = `UPDATE users SET name = ?, email = ?,password = ? WHERE id = ${user_id} `;
+        const query = `UPDATE users SET name = ${name}, email = ${email},password = ${password} WHERE id = ${user_id} `;
 
-        await provider.promise().query(query, [email, name, password]);
+        await provider.promise().query(query);
 
         res.redirect(`users/${user_id}`);
 
@@ -15,3 +14,4 @@ module.exports = async (req, res) => {
         res.status(400).json(e.message);
     }
 };
+
